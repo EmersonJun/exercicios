f0503ac38c033161ac5de765fc5bc69ac1fe7179
Emerson <emersonnjunior2006@gmail.com>
2025-03-26 13:35:51 -0300

    exercicio java 01

---
+++ src/exercicio1/Calculadora.java
@@ -0,0 +1,86 @@
+package exercicio1;
+public class Calculadora {
+    private float numero1, numero2, resultado;
+
+    public Calculadora(float numero1, float numero2) {
+        this.numero1 = numero1;
+        this.numero2 = numero2;
+        this.resultado = 0;
+    }
+
+    public float soma(){
+        if (numero1 <50 || numero1>0 && numero2 <50 || numero2>0) {
+        resultado = numero1 + numero2;
+        }
+        return resultado;
+    }
+    public float subtracao(){
+        if (numero1 <50 || numero1>0 && numero2 <50 || numero2>0) {
+        resultado = numero1 - numero2;
+        }
+        return resultado;
+    }
+    public float multiplicacao(){
+        if (numero1 <50 || numero1>0 && numero2 <50 || numero2>0) {
+        resultado = numero1 * numero2;
+        }
+        return resultado;
+    }
+    public float divisao(){
+        if (numero1 <50 || numero1>0 && numero2 <50 || numero2>0) {
+            if (numero2 == 0) {
+                throw new ArithmeticException("Divisão por zero não é permitida.");
+            }
+            resultado = numero1 / numero2;
+        }
+        return resultado;
+    }
+
+    public float potenciacao(){
+        if (numero1 <50 || numero1>0 || numero2 <50 || numero2>0) {
+            if (numero2 == 0) {
+                throw new ArithmeticException("potenciacao por zero não é permitida.");
+            }
+            resultado = 1;
+            for(int i=0; i<numero2; i++){
+                resultado*=numero1;
+            }
+        }
+        return resultado;
+    }
+
+    public float fatorial(){
+        if (numero1 < 0 || numero1 > 50) {
+            throw new IllegalArgumentException("O fatorial só é permitido para números entre 0 e 50.");
+        }
+
+        int fatorial = 1;
+        for (int i = 1; i <= numero1; i++) {
+            fatorial *= i;
+        }
+        return fatorial;
+    }
+
+    public float getNumero1() {
+        return numero1;
+    }
+
+    public void setNumero1(float numero1) {
+        this.numero1 = numero1;
+    }
+
+    public float getNumero2() {
+        return numero2;
+    }
+
+    public void setNumero2(float numero2) {
+        this.numero2 = numero2;
+    }
+
+    @Override
+    public String toString() {
+        return "Calculadora [numero1=" + numero1 + ", numero2=" + numero2 + "]";
+    }
+    
+    
+}
---
+++ src/exercicio2/Livro.java
@@ -0,0 +1,52 @@
+package exercicio2;
+
+import java.time.LocalDate;
+
+public class Livro {
+    private String nome, titulo;
+    private int anoDePublicacao;
+
+    public Livro(String nome, String titulo, int anoDePublicacao) {
+        this.nome = nome;
+        this.titulo = titulo;
+        this.anoDePublicacao = anoDePublicacao;
+    }
+
+    public void exibirInformacoes(String nome, String titulo, LocalDate anoDePublicacao){
+        System.out.println(nome);
+        System.out.println(titulo);
+        System.out.println(anoDePublicacao);
+    }
+
+    public int idadeDoLivro(){
+        int anoAtual = LocalDate.now().getYear();  
+        return anoAtual - anoDePublicacao;
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public int getAnoDePublicacao() {
+        return anoDePublicacao;
+    }
+    public void setAnoDePublicacao(int anoDePublicacao) {
+        this.anoDePublicacao = anoDePublicacao;
+    }
+    @Override
+    public String toString() {
+        return "Livro [nome=" + nome + ", titulo=" + titulo + ", anoDePublicacao=" + anoDePublicacao + "]";
+    }
+    
+
+    
+}
